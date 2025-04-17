6f0a6fd34ce6b68571d2db42fc1804769388c5ae
KenanAljaber <keno12333@hotmail.com>
2025-04-17 09:12:46 +0200

    Global baisc apiResponse class

---
+++ src/utils/apiResponse.js
@@ -0,0 +1,22 @@
+// utils/ApiResponse.ts
+
+export class ApiResponse {
+    static success(res, data = null, message = "Success", statusCode = 200) {
+      return res.status(statusCode).json({
+        success: true,
+        message,
+        data,
+      });
+    }
+  
+    static error(res, error = null, statusCode = 500) {
+      console.error("API Error:", error);
+      const  message = error.message || "An error occurred" ;
+      return res.status(statusCode).json({
+        success: false,
+        message,
+        error: error?.message || error || null,
+      });
+    }
+  }
+  
