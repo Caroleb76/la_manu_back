7ac5aa7d4cd52be692759a100fa2bc9e7f88ad75
Enerco76 <contact.enerco@gmail.com>
2025-04-18 10:41:41 +0200

    added missing routes

--- src/routes.js
+++ src/routes.js
@@ -4,4 +4,9 @@ import auth from "./controllers/auth/index.js";
 export default (router) => {
   router.use("/users", users); // une route pour chaque répertoire ex users contrats ...
   router.use("/auth",auth ); 
+  router.use("/files", files);
+  router.use("/addresses", addresses);
+  router.use("/roles", roles);
+  router.use("/extraCosts", extraCosts);
+  
 };
