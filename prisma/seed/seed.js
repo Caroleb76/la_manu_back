6d55274d603ffd2c54463e5958c39fa4c18f51d8
loic grancher <loic.grancher@protonmail.com>
2025-05-16 16:35:39 +0200

    refactored seeders

--- prisma/seed/seed.js
+++ prisma/seed/seed.js
@@ -1,6 +1,8 @@
 import { PrismaClient } from "@prisma/client";
 import { ROLES } from "../../src/utils/constants.js";
 import utils from "../../src/utils/utils.js";
+import createNotifications from "./notificationSeed.js";
+import createUsers  from "./userSeed.js";
 
 const prisma = new PrismaClient();
 
@@ -20,87 +22,14 @@ async function main() {
     }
     //create users
     await createUsers(createdRoles);
+    console.log("users seeded");
+
     await createNotifications();
-    console.log("database seeded");
+    console.log("notifications seeded");
+
 }
 
-async function createUsers(roles) {
-    let superAdminsEmails = [
-        "kenan@demo.com",
-        "loic@demo.com",
-        "carole@demo.com",
-    ];
-    let adminsEmails = [
-        "admin1@demo.com",
-        "admin2@demo.com",
-        "admin3@demo.com",
-    ];
-    let formateurEmails = [
-        "formateur1@demo.com",
-        "formateur2@demo.com",
-        "formateur3@demo.com",
-    ];
-    const globalPassword = "password";
-    const hashedPassword = await utils.hashPassword(globalPassword);
-    // creation de SUPER_ADMIN
-    for (let email of superAdminsEmails) {
-        let currentRole = roles[ROLES.SUPER_ADMIN];
-        await createUsersByRole(currentRole.id, email, hashedPassword);
-    }
 
-    // creation de ADMIN
-    for (let email of adminsEmails) {
-        let currentRole = roles[ROLES.ADMIN];
-        await createUsersByRole(currentRole.id, email, hashedPassword);
-    }
-
-    // creation de FORMATEUR
-    for (let email of formateurEmails) {
-        let currentRole = roles[ROLES.FORMATEUR];
-        await createUsersByRole(currentRole.id, email, hashedPassword);
-    }
-}
-
-async function createUsersByRole(role, email, password) {
-    await prisma.user.upsert({
-        where: { email: email },
-        update: {},
-        create: {
-            email: email,
-            password: password,
-            roleId: role,
-            firstName: email.split("@")[0],
-        },
-    });
-}
-
-//  id        String   @id @default(uuid())
-//   title     String
-//   content   String?
-//   priority  Int      @default(1)
-//   startDate DateTime?
-//   endDate   DateTime?
-
-async function createNotifications() {
-    await prisma.notification.createMany({
-        data: [
-            {
-                title: "Notification 1",
-                content: "Description de la notification 1",
-                priority: 1,
-                startDate: new Date("2023-01-01"),
-                endDate: new Date("2023-01-31"),
-            },
-            {
-                title: "Notification 2",
-                content: "Description de la notification 2",
-                priority: 2,
-                startDate: new Date("2023-02-01"),
-                endDate: new Date("2023-02-25"),
-            },
-        ],
-    });
-}
 
 main()
     .then(async () => {
---
+++ prisma/seed/userSeed.js
@@ -0,0 +1,52 @@
+
+ async function createUsers(roles) {
+    let superAdminsEmails = [
+        "kenan@demo.com",
+        "loic@demo.com",
+        "carole@demo.com",
+    ];
+    let adminsEmails = [
+        "admin1@demo.com",
+        "admin2@demo.com",
+        "admin3@demo.com",
+    ];
+    let formateurEmails = [
+        "formateur1@demo.com",
+        "formateur2@demo.com",
+        "formateur3@demo.com",
+    ];
+    const globalPassword = "password";
+    const hashedPassword = await utils.hashPassword(globalPassword);
+    // creation de SUPER_ADMIN
+    for (let email of superAdminsEmails) {
+        let currentRole = roles[ROLES.SUPER_ADMIN];
+        await createUsersByRole(currentRole.id, email, hashedPassword);
+    }
+
+    // creation de ADMIN
+    for (let email of adminsEmails) {
+        let currentRole = roles[ROLES.ADMIN];
+        await createUsersByRole(currentRole.id, email, hashedPassword);
+    }
+
+    // creation de FORMATEUR
+    for (let email of formateurEmails) {
+        let currentRole = roles[ROLES.FORMATEUR];
+        await createUsersByRole(currentRole.id, email, hashedPassword);
+    }
+}
+
+export async function createUsersByRole(role, email, password) {
+    await prisma.user.upsert({
+        where: { email: email },
+        update: {},
+        create: {
+            email: email,
+            password: password,
+            roleId: role,
+            firstName: email.split("@")[0],
+        },
+    });
+}
+
+export default createUsers;
