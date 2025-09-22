cd3696cfbd56ed7b314d43b0f0e1fc83b6385e8c
Kenan Aljaber <keno12333@hotmail.com>
2025-09-22 09:45:38 +0200

    fix gitignore

---
+++ src/controllers/files/getAllByExtraCostsId.js
@@ -0,0 +1,12 @@
+import fileService from "../../services/fileService.js";
+import ApiResponse  from "../../utils/apiResponse.js";
+export default async (req, res) => {
+    try {
+      const userId = req.params.userId;
+      const extraCostId = req.params.extraCostId;
+      const users= await fileService.getFilesByExtraCostId(extraCostId);
+      ApiResponse.success(res,users);
+    } catch (error) {
+      return ApiResponse.error(res, error);
+    }
+  };
